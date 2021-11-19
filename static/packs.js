dcd09b82b76ee87de3ae6c895095c02b0ca33104
Jay Minton <mint7780@gmail.com>
2021-11-19 00:13:57 -0800

    added baseline style changes

--- static/packs.js
+++ static/packs.js
@@ -8,6 +8,14 @@ $("li form input").on("click", async function (evt) {
     let pack_code= $(this).parent().attr('id');
     let pack_name = $(this).parent().attr('name');
     console.log(pack_name, pack_code);
+    if(this.value == "add") {
+        this.value = "remove";
+        $(this).addClass("pack-clicked");
+    }
+    else {
+        this.value = "add";
+        $(this).removeClass("pack-clicked");
+    }
 
     let addedToCollection = await axios.post(`${BASE_URL}/collection/add`, {
         pack_code,
